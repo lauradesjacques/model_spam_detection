a5d992ef5bd9d93ad5aa2890e936466c130389de
patricio salfati <p.salfati@gmail.com>
2023-05-26 11:15:59 +0200

    adding azure script

--- app.py
+++ app.py
@@ -1,5 +1,23 @@
-import pandas as pd
-import numpy as pd
 
-print('hello')
-application uvicorn:app --reload
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get('/')
+def index():
+    '''
+    This is a first docstring.
+    '''
+    return {'message': 'Hello, stranger'}
+
+@app.get('/{name}')
+def get_name(name: str):
+    '''
+    This is a second docstring.
+    '''
+    return {'message': f'Hello, {name}'}
+
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='127.0.0.1', port=8000)
